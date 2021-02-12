0be99495d90a8ef981b2b4bf0280dee03de5023c
Crowdbotics <team@crowdbotics.com>
2021-02-12 05:49:53 +0000

    Update models

--- backend/faster_drivers_data_24466/wsgi.py
+++ backend/faster_drivers_data_24466/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'faster_drivers_data_24466.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "faster_drivers_data_24466.settings")
 
 application = get_wsgi_application()
